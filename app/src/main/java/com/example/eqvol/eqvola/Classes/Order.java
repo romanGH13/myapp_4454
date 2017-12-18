26961ec4671395b9edb1c36dce0620e5537e70fe
Роман Трегуб <romatregub@gmail.com>
2017-12-18 18:10:26 +0200

    Orders Fragments

---
+++ app/src/main/java/com/example/eqvol/eqvola/Classes/Order.java
@@ -0,0 +1,8 @@
+package com.example.eqvol.eqvola.Classes;
+
+/**
+ * Created by eqvol on 18.12.2017.
+ */
+
+public class Order {
+}
---
+++ app/src/main/java/com/example/eqvol/eqvola/JsonResponse/JsonResponseOrders.java
@@ -0,0 +1,8 @@
+package com.example.eqvol.eqvola.JsonResponse;
+
+/**
+ * Created by eqvol on 18.12.2017.
+ */
+
+public class JsonResponseOrders {
+}

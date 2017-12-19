794e3ecd501356f643d90469a2b34c64bdc9cbd8
Роман Трегуб <romatregub@gmail.com>
2017-12-19 18:00:27 +0200

    notification v1.0

---
+++ app/src/main/java/com/example/eqvol/eqvola/Classes/Notification.java
@@ -0,0 +1,8 @@
+package com.example.eqvol.eqvola.Classes;
+
+/**
+ * Created by eqvol on 19.12.2017.
+ */
+
+public class Notification {
+}
---
+++ app/src/main/java/com/example/eqvol/eqvola/JsonResponse/JsonResponseNotification.java
@@ -0,0 +1,8 @@
+package com.example.eqvol.eqvola.JsonResponse;
+
+/**
+ * Created by eqvol on 19.12.2017.
+ */
+
+public class JsonResponseNotification {
+}
